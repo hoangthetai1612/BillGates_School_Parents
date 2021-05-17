67204bea265c9399249e53b058a8409b4fe6b9ea
nambv07421 <nambui377@gmail.comm>
2021-05-17 17:07:55 +0700

    update

--- src/app/base/header/header.component.ts
+++ src/app/base/header/header.component.ts
@@ -1,14 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+interface HeaderStyle {
+  cssClass: string;
+  classText: string;
+  iconLeft: any;
+  iconRight: any;
+  iconCenter: any;
+  typecenter: any;
 
+
+}
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @Input() headerStyle: HeaderStyle;
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
 }
+
+
+
+@NgModule({
+  declarations: [HeaderComponent],
+  imports: [
+    CommonModule,
+    IonicModule
+  ],
+  exports: [HeaderComponent]
+})
+export class HeaderModule { }
