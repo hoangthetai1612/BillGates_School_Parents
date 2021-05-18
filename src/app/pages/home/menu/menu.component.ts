b87477b59c598beaa7a3613b0d4b46a05ed9d9d0
nambv07421 <nambui377@gmail.comm>
2021-05-18 17:10:39 +0700

    .

--- src/app/pages/home/menu/menu.component.ts
+++ src/app/pages/home/menu/menu.component.ts
@@ -1,7 +1,11 @@
-import { CommonModule } from '@angular/common';
-import { Component, NgModule, OnInit } from '@angular/core';
+import { CommonModule, registerLocaleData } from '@angular/common';
+import { Component, LOCALE_ID, NgModule, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { HeaderModule } from 'src/app/base/header/header.component';
+import startOfWeek from 'date-fns/startOfWeek';
+import eachDayOfInterval from 'date-fns/eachDayOfInterval';
+import localeVi from "@angular/common/locales/vi";
+registerLocaleData(localeVi);
 
 @Component({
   selector: 'app-menu',
@@ -23,10 +27,58 @@ export class MenuComponent implements OnInit {
     }
 
   };
+  list = [0, 1, 2, 3, 4];
+  startWeek: Date;
+  endWeek: Date;
+  currentDate = new Date();
+  listDate = [];
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.startWeek = startOfWeek(new Date(), { weekStartsOn: 1 });
+    this.endWeek = new Date(
+      this.startWeek.getFullYear(),
+      this.startWeek.getMonth(),
+      this.startWeek.getDate() + 5
+    );
+    this.listDate = eachDayOfInterval({
+      start: this.startWeek,
+      end: this.endWeek,
+    });
 
+  }
+  nextWeek() {
+    this.startWeek = new Date(
+      this.startWeek.getFullYear(),
+      this.startWeek.getMonth(),
+      this.startWeek.getDate() + 7
+    );
+    this.endWeek = new Date(
+      this.startWeek.getFullYear(),
+      this.startWeek.getMonth(),
+      this.startWeek.getDate() + 5
+    );
+    this.listDate = eachDayOfInterval({
+      start: this.startWeek,
+      end: this.endWeek,
+    });
+  }
+  preWeek() {
+    this.startWeek = new Date(
+      this.startWeek.getFullYear(),
+      this.startWeek.getMonth(),
+      this.startWeek.getDate() - 7
+    );
+    this.endWeek = new Date(
+      this.startWeek.getFullYear(),
+      this.startWeek.getMonth(),
+      this.startWeek.getDate() + 5
+    );
+    this.listDate = eachDayOfInterval({
+      start: this.startWeek,
+      end: this.endWeek,
+    });
+  }
 }
 @NgModule({
   declarations: [MenuComponent],
@@ -35,7 +87,9 @@ export class MenuComponent implements OnInit {
     IonicModule,
     HeaderModule
   ],
-  exports: [MenuComponent]
+  exports: [MenuComponent],
+  providers: [{ provide: LOCALE_ID, useValue: 'vi-VN' }],
+  entryComponents: [MenuComponent],
 })
 export class MenuModule {
 
