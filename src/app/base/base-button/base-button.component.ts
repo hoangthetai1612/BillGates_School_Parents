4950bdaab074009cc58016718850960b663b4a2b
nambv07421 <nambui377@gmail.comm>
2021-05-18 11:30:46 +0700

    update

--- src/app/base/base-button/base-button.component.ts
+++ src/app/base/base-button/base-button.component.ts
@@ -1,14 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, NgModule, OnInit, Output } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+interface ButtonStyle {
+  width: string;
+  cssClass?: string;
+  text?: string;
+  disable?: boolean;
+}
 @Component({
   selector: 'app-base-button',
   templateUrl: './base-button.component.html',
   styleUrls: ['./base-button.component.scss'],
 })
 export class BaseButtonComponent implements OnInit {
-
+  @Input() buttonStyle: ButtonStyle;
+  @Input() data: any;
+  @Output() callback = new EventEmitter();
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
+  handleCallbackEvent = (type) => {
+    this.callback.emit(type);
+  };
+}
+@NgModule({
+  declarations: [BaseButtonComponent],
+  imports: [
+    CommonModule,
+    IonicModule
+  ],
+  exports: [BaseButtonComponent]
+})
+export class BaseButtonModule {
 
 }
--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -7,14 +7,16 @@ import { IonicModule } from '@ionic/angular';
 import { ProfilePageRoutingModule } from './profile-routing.module';
 
 import { ProfilePage } from './profile.page';
+import { BaseButtonModule } from 'src/app/base/base-button/base-button.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ProfilePageRoutingModule
+    ProfilePageRoutingModule,
+    BaseButtonModule
   ],
   declarations: [ProfilePage]
 })
-export class ProfilePageModule {}
+export class ProfilePageModule { }
