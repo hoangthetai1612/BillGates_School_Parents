871404abbca1445a1ef77415d8a88293672a690f
Nguyentriphuong <Nguyentriphuong2107@gmail.com>
2021-05-24 15:38:30 +0700

    update

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NewModel } from 'src/app/models/new.model';
+import { ApiService } from 'src/app/service/api.service';
 
 @Component({
   selector: 'app-home',
@@ -19,39 +22,36 @@ export class HomePage implements OnInit {
       isText: true,
     },
   };
-  data = [
-    {
-      mediaURL: 'assets/svg/image-post.svg',
-      title: 'Khai giảng khóa học kỹ năng sống cho trẻ từ 3-6 tuổi',
-      date: '12/12/20',
-    },
-    {
-      mediaURL: 'assets/svg/image-post.svg',
-      title: 'Khai giảng khóa học kỹ năng sống cho trẻ từ 3-6 tuổi',
-      date: '12/12/20',
-    },
-    {
-      mediaURL: 'assets/svg/image-post.svg',
-      title: 'Khai giảng khóa học kỹ năng sống cho trẻ từ 3-6 tuổi',
-      date: '12/12/20',
-    },
-    {
-      mediaURL: 'assets/svg/image-post.svg',
-      title: 'Khai giảng khóa học kỹ năng sống cho trẻ từ 3-6 tuổi',
-      date: '12/12/20',
-    },
-    {
-      mediaURL: 'assets/svg/image-post.svg',
-      title: 'Khai giảng khóa học kỹ năng sống cho trẻ từ 3-6 tuổi',
-      date: '12/12/20',
-    },
-    {
-      mediaURL: 'assets/svg/image-post.svg',
-      title: 'Khai giảng khóa học kỹ năng sống cho trẻ từ 3-6 tuổi',
-      date: '12/12/20',
-    },
-  ];
-  constructor() {}
+  data: NewModel[] = [];
+  recv: any;
+  constructor(private router: Router, private apiService: ApiService) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.setNews();
+  }
+  setNews() {
+    const dataObject: NewModel = new NewModel();
+    dataObject.Title = 'Khai giảng khóa học kỹ năng sống cho trẻ từ 3-6 tuổi';
+    dataObject.CreatedByName = 'Anh';
+    dataObject.MediaURL = 'assets/svg/image-post.svg';
+    dataObject.Description = 'Khai giảng khóa học kỹ năng sống cho trẻ từ 3-6 tuổi';
+    dataObject.URLLink = 'contact-book';
+    dataObject.CreatedOn = new Date();
+    for (let i = 0; i < 10; i++) {
+      this.data.push(dataObject);
+    }
+    let paramReq = {
+      pageNumber: 1,
+      pageSize: 20
+    }
+    this.apiService.get('news', paramReq)
+      .subscribe(n => this.recv = n, null, () => {
+        this.data = this.recv;
+      });
+  }
+  open() {
+    console.log("đs");
+
+    this.router.navigateByUrl('');
+  }
 }
