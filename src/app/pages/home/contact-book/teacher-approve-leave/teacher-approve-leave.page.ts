fde935eea562952c3092eb5a52c63f4412ad9534
Duoc <duocpv@consultindochina.com>
2021-05-28 13:34:33 +0700

    remove fake-data

--- src/app/pages/home/contact-book/teacher-approve-leave/teacher-approve-leave.page.ts
+++ src/app/pages/home/contact-book/teacher-approve-leave/teacher-approve-leave.page.ts
@@ -23,30 +23,6 @@ export class TeacherApproveLeavePage implements OnInit {
     }
   };
 
-  data = [
-    {
-      createdDate: '15/05/2021',
-      name: 'Phùng Thanh Hưng',
-      startDate: '8:00 16/05/2021',
-      endDate: '17:00 18/05/2021',
-      description: `Gia đình em có kế hoạch đi du lịch vào cuối tuần tới. 
-      Nên em viết đơn xin được nghỉ học 2 ngày cuối tuần. 
-      Em xin hứa sẽ học và chép bài đầy đủ mà không làm ảnh hưởng đến việc học tập của mình`,
-      status: 1,
-      isDetail: false
-    },
-    {
-      createdDate: '15/05/2021',
-      name: 'Nguyễn Khánh Huyền',
-      startDate: '8:00 16/05/2021',
-      endDate: '17:00 18/05/2021',
-      description: `Gia đình em có kế hoạch đi du lịch vào cuối tuần tới. 
-      Nên em viết đơn xin được nghỉ học 2 ngày cuối tuần. 
-      Em xin hứa sẽ học và chép bài đầy đủ mà không làm ảnh hưởng đến việc học tập của mình`,
-      status: 2,
-      isDetail: false
-    }
-  ];
   absences = [];
   teacher: TeacherModel;
   classId: number;
