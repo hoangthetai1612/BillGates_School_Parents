df5ba7d044c4f20f89e5f61859708453dae2e617
Duoc <duocpv@consultindochina.com>
2021-05-28 09:04:14 +0700

    add student service

---
+++ src/app/service/student.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { StudentModel } from '../models/student.model';
+import { BaseApiService } from './base.service';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class StudentService extends BaseApiService<StudentModel[]> {
+    constructor(protected http: HttpClient) {
+        super(http, 'api/student');
+    }
+
+    getStudentsByTeacher(): Observable<StudentModel[]> {
+        return this.http.get<StudentModel[]>('api/student/username/teacher').pipe(map((res: any) => res.Payload));
+    }
+}
